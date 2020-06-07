ec3cd874889f089cf998046c10a90138143b4707
unknown <malik.aksh07@gmail.com>
2020-06-07 09:27:49 +0530

    Third commit

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+const port=process.env.PORT||3000
 
 var htmlPath = path.join(__dirname, 'html');
 
 app.use(express.static(htmlPath));
 
-var server = app.listen(3000, function () {
-    var host = 'localhost';
-    var port = server.address().port;
-    console.log('listening on http://'+host+':'+port+'/');
-});
+app.listen(port, function(){
+    console.log('Port started')
+})
